ab8553ef1e6665d4b315360d3df3382f3049d938
nicola <nicolaaprile123@gmail.com>
2025-01-14 16:15:00 +0100

    Gamma 0.1

---
+++ src/test/java/com/generation/postinoviaggiatore/PostinoViaggiatoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.postinoviaggiatore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostinoViaggiatoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
